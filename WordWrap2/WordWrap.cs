0bbe93a470ee5301676b2fc770712661de78922c
MayerlySalas0615 <mayerly.salas@sinco.com.co>
2025-11-20 15:02:26 -0500

    🟥 b

--- WordWrap2/WordWrap.cs
+++ WordWrap2/WordWrap.cs
@@ -11,6 +11,15 @@ public class WordWrap
 
         result.Should().Be("");
     }
+    
+    [Fact]
+    public void b()
+    {
+        var result = Wrap("this", 10);
+
+        result.Should().Be("this");
+    }  
+    
     private static string Wrap(string text, int col)
     {
         return "";
