ebad48e430deb6dd93996592fd729e3f19975736
MayerlySalas0615 <mayerly.salas@sinco.com.co>
2025-11-20 15:00:46 -0500

    🟥 WordWrap

---
+++ WordWrap2/WordWrap.cs
@@ -0,0 +1,18 @@
+﻿using AwesomeAssertions;
+
+namespace WordWrap2;
+
+public class WordWrap
+{
+    [Fact]
+    public void a()
+    {
+        var result = Wrap("", 1);
+
+        result.Should().Be("");
+    }
+    private static string Wrap(string text, int col)
+    {
+        throw new Exception();
+    }
+}
